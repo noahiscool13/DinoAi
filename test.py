0df9024cb7e0cc6254a97e9b8fb074924818e30e
njadoenathmisi <n.j.m.jadoenathmisier@student.tudelft.nl>
2020-08-27 19:42:03 +0200

    c1

--- test.py
+++ test.py
@@ -1,23 +1,36 @@
 import pyautogui
+import time
+import PIL.ImageGrab as ImageGrab
 
 restart_button = pyautogui.locateCenterOnScreen("restart.bmp")
-dino = pyautogui.locateCenterOnScreen("dino.png")
-print(dino)
-print(restart_button)
-
-# pyautogui.click(475,196)
 pyautogui.click(restart_button)
+pyautogui.sleep(1)
+dino = pyautogui.locateCenterOnScreen("dino.png")
 
-loc = (dino[0]+60,dino[1]+5)
+front_locs = [(dino[0]+75+i,dino[1]+15) for i in range(30)]
 
-pyautogui.moveTo(loc)
-pyautogui.sleep(3)
+box_area1 = (dino[0]+85,dino[1]+10,dino[0]+95, dino[1]+15)
+
+pyautogui.moveTo(front_locs[0])
+
+
 
 while 1:
+    time1 = time.time()
+    box = ImageGrab.grab(box_area1)
 
-    if pyautogui.pixel(*loc) != (255,255,255):
+    if any(box.getpixel((i*2,2))== (83,83,83) for i in range(1,5)):
         pyautogui.press("space")
+        print("jump")
+
+    time2 = time.time()
+    print(1/(time2-time1))
+
+    # if any(pyautogui.pixel(*loc) == (83,83,83) for loc in front_locs):
+    #     pyautogui.press("space")
+    #     print("jump")
     # print(pyautogui.pixel(*loc))
+    # print("!")
 
 
 
